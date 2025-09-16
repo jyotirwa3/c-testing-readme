4bfeb4ededc32ea2e3bab20a4b4eb636fab71baf
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-09-16 17:23:13 +0530

    testing

---
+++ new-line.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("hello\n");
+    printf("world");
+    printf("\nweb");
+    printf("\tdev");
+    printf("       dev");
+    return 0;
+}
---
+++ new-line.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "hello\n";
+    cout << "world" << endl;
+    cout << "web\t";
+    cout << "test";
+    return 0;
+}
